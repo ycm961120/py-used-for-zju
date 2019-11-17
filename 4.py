8d362ebac49872680b9a7a67ff1c3945372aef97
ycm961120 <56991432+ycm961120@users.noreply.github.com>
2019-11-17 11:05:00 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -16,6 +16,8 @@ std = train_data.std(axis=0)
 train_data -= mean
 train_data /= std
 
+
+
 #生成模型
 def build_model():
     model = models.Sequential()
@@ -30,10 +32,27 @@ def build_model():
 #训练模型
 model = build_model()
 model.fit(train_data,train_targets,
-          epochs=200,batch_size=2,verbose=0)
+          epochs=300,batch_size=2,verbose=0)
 
-del train_data
-del train_targets
+
+
+#变量重要性
+#基于MIV的相关变量检测，MIV（mean impact value）平均影响值算法，主要用于检测哪些输入变量与输出相关
+#算法很简单，先训练好网络。再将某个输入减少10%和增加10%（其它输入保持不变），将两组数据都输入到网络中，
+#查看该输入的落差会引起网络输出多少的落差。若果引起的落差很少，则说明该输入对输出影响很小，
+#否则，则认为对输出影响较大。
+#MIV被认为是在神经网络中评价变量相关的最好指标之一，其符号代表相关的方向,increase-decrease
+result = []
+for i in range(train_data.shape[1]):
+    inputdata1 = np.copy(train_data)
+    inputdata1[:,i] *=  0.9
+    inputdata2 = np.copy(train_data)
+    inputdata2[:,i] *= 1.1
+    pre1 = model.predict(inputdata1)
+    pre2 = model.predict(inputdata2)
+    result.append((pre2-pre1).mean(axis=0)[0])
+print(result)
+
 
 #标准化输入数据
 ls = ['0','1','2','4','5','6','7','10','11','13','14','15','17','18','19']
