f0176dcb17a16dcb579724c0e19dc5749827eec3
ycm961120 <56991432+ycm961120@users.noreply.github.com>
2020-02-20 22:05:42 +0800

    Update PythonServer.py

--- PythonServer.py
+++ PythonServer.py
@@ -9,14 +9,15 @@ Created on Tue Feb  4 11:43:01 2020
 并用于给java提供调用
 
 与java的连接部分已经完成，你要做的只是将你想要通过python实现的算法，
-写到88行开始的大量###处即可。
+重写62行的run方法即可。
 """
 
 import socket
 import sys
 import threading
-import numpy as np
-from PIL import Image
+import arcpy
+import os
+
 
 def main():
     # 创建服务器套接字
@@ -51,7 +52,6 @@ def main():
 
 
 class ServerThreading(threading.Thread):
-    # words = text2vec.load_lexicon()
     def __init__(self,clientsocket,recvsize=1024*1024,encoding="GBK"):
         threading.Thread.__init__(self)
         self._socket = clientsocket
@@ -74,40 +74,44 @@ class ServerThreading(threading.Thread):
                 if msg.strip().endswith('over'):
                     msg=msg[:-4]
                     break
-            print(msg)
-            """
-            sendmsg = Image.open(msg)
-            # 发送数据
-            self._socket.send(("%s"%sendmsg).encode(self._encoding))
-            pass
-            """
-            self._socket.send(("路径收到！胜利的法则决定了").encode(self._encoding))
-            
-            
-            
-            """
-            print("hello world")
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            ###########################
-            """
+            way = msg.split(",")
+            dataWay = way[1]
+            saveWay = way[2] + "/trash"
+            #os.mkdir(saveWay)
+
+            #将dataWay下的栅格图像裁剪成5*10块，保存到saveWay下的trash文件夹中,顺便统计下有几个变量
+            count = 0;
+            for file in os.listdir(dataWay):
+                count += 1
+                way1 = dataWay+"/"+file
+
+                #arcpy.arcpy.SplitRaster_management(way1,saveWay,\
+                #                        file.split(".")[0], "NUMBER_OF_TILES", "TIFF", "NEAREST",\
+                #                        "5 10", "#", "4", "PIXELS", "#", "#")
+
+            for i in range(50):
+                data = ""
+                for file in os.listdir(saveWay):
+                    if file.endswith(str(i) + ".TIF"):
+                        if i < 10 and not ("0"<=file[-len(str(i))-5]<="9"):
+                            data += saveWay+"/"+file+","
+                        if i >=10:
+                            data += saveWay + "/" + file + ","
+                data = data[:-1]
+                if len(data.split(",")) == count:
+                    print(data)
+                    self._socket.send(data.encode(self._encoding))
+                    self._socket.send(";".encode(self._encoding))
+
+
+
             pass
         
         except Exception as identifier:
             self._socket.send("500".encode(self._encoding))
             print(identifier)
             pass
+        
         finally:
             self._socket.close() 
         print("任务结束.....")
@@ -119,4 +123,3 @@ class ServerThreading(threading.Thread):
 
 if __name__ == "__main__":
     main()
-    
